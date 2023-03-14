0dbe3c9ea93f14e2f6ff4dc334a8e4d3ef7768cd
oOTEEOo <thanadon.kr@ku.th>
2023-03-14 21:03:19 +0700

    commit for pull

--- project/main/main.ino
+++ project/main/main.ino
@@ -1,5 +1,16 @@
 #include <MusiciansMate.h>
 
+/* Fill-in information from Blynk Device Info here */
+#define BLYNK_TEMPLATE_ID           "TMPLAllvhdfB"
+#define BLYNK_TEMPLATE_NAME         "Quickstart Template"
+#define BLYNK_AUTH_TOKEN            "3_95z6wnvK7gr9qP2j72ZGALA3osxOTt"
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
 #define BUZZ_PIN 17
 #define JOYSTICK_PIN 16
 #define DEFAULT_TEMPO 120
@@ -8,6 +19,42 @@
 #define TUNER_DURATION (1000)
 
 #define DEBOUNCE_DELAY (unsigned long) 50
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "TEE";
+char pass[] = "Tee00000";
+
+BlynkTimer timer;
+
+// This function is called every time the Virtual Pin 0 state changes
+BLYNK_WRITE(V0)
+{
+  // Set incoming value from pin V0 to a variable
+  int value = param.asInt();
+
+  // Update state
+  Blynk.virtualWrite(V1, value);
+}
+
+// This function is called every time the device is connected to the Blynk.Cloud
+BLYNK_CONNECTED()
+{
+  // Change Web Link Button message to "Congratulations!"
+  Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
+  Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
+  Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
+}
+
+// This function sends Arduino's uptime every second to Virtual Pin 2.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V2, millis() / 1000);
+}
+
+
 
 TaskHandle_t InputMonitor;
 
@@ -35,7 +82,16 @@ GuitarTuner *tuner = (GuitarTuner *) TunerBuilder::build(BUZZ_PIN, GUITAR, TUNER
 
 void setup()
 {
+     // Debug console
     Serial.begin(115200);
+
+    // You can also specify server:
+    //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+    //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+    // Setup a function to be called every second
+    timer.setInterval(1000L, myTimerEvent);
+    
 
     pinMode(BUZZ_PIN, OUTPUT);
     pinMode(JOYSTICK_PIN, INPUT_PULLUP);
@@ -62,6 +118,11 @@ void setup()
 
 void loop()
 {
+    Blynk.run();
+    timer.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
     if (metronome->getIsPlaying() == false && metronomeState > 0)
         metronome->start();
 }
