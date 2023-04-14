ac5ee199f72083d5bdb0a94e67c9c7cd0195cae1
Potsawat Thinkanwatthana <potsawattkw@outlook.com>
2023-04-14 20:50:12 +0700

    chore: Cleanup

--- project/setup.sh
+++ project/setup.sh
@@ -1,7 +1,3 @@
 #! /usr/bin/env bash
 
-arduino-cli lib install ArduinoIoTCloud LiquidCrystal_I2C
-
-export ARDUINO_LIBRARY_ENABLE_UNSAFE_INSTALL=true
-arduino-cli lib install --git-url https://github.com/naiithink/musicians-mate.git
-unset ARDUINO_LIBRARY_ENABLE_UNSAFE_INSTALL
+arduino-cli lib install ArduinoIoTCloud LiquidCrystal_I2C MusiciansMate
